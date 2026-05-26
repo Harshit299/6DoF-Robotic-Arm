96b759e701526149f1bd9a9e3ee576460d4da6f3
Harshit K Mahaur <hkmnnit369@gmail.com>
2026-05-26 21:27:40 +0530

    Corrected typo (34 to 24)

--- 6dof_arm/src/robot_arm/robot_arm/Joint_Space_Cubic_Trajectory.py
+++ 6dof_arm/src/robot_arm/robot_arm/Joint_Space_Cubic_Trajectory.py
@@ -7,10 +7,10 @@ from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 
 
-# Link lengths
+# Link offsets
 a1, a2, a3 = 7.5, 30.0, 7.5
 
-# Link offsets
+# Link lengths
 d1, d2, d3 = 33.0, 32.0, 24.0
 
 class TrajectoryNode(Node):
